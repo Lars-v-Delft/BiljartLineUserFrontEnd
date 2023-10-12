545d72b405fc4d191550c68966393d3d471e8b82
Lars-v-Delft <lars171998@gmail.com>
2023-10-12 15:36:25 +0200

    API URI string fix

--- app/services/competitions.tsx
+++ app/services/competitions.tsx
@@ -2,7 +2,7 @@
 
 
 export async function getCompetitionsByFederationId() {
-    const response = await fetch('http://localhost:8080/competition/byFederation?federationId=1', { cache: "no-cache" })
+    const response = await fetch('http://localhost:8080/competition/byFederation?id=1', { cache: "no-cache" })
     if (!response.ok) {
         throw new Error('failed to fetch competitions')
     }
